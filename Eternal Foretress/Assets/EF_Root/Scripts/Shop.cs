76e3ab0a88a3d1a923cc702021e3b301dfa13d0c
AsierMUI <asieravino@gmail.com>
2025-05-24 19:16:47 +0200

    a

---
+++ Eternal Foretress/Assets/EF_Root/Scripts/Shop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandarTurret()
+    {
+        Debug.Log("Standar Turret Purchased");
+    }
+
+    public void PurchaseDemonTurret()
+    {
+        Debug.Log("Demon Turret Purchased");
+    }
+
+}
