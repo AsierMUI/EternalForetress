3978860e4d3de23b6bd065ed71fd9c4daed4a435
AsierMUI <asieravino@gmail.com>
2025-05-25 17:27:35 +0200

    a

--- Eternal Foretress/Assets/EF_Root/Scripts/other/Waypoint.cs
+++ Eternal Foretress/Assets/EF_Root/Scripts/other/Waypoint.cs
@@ -8,9 +8,9 @@ public class Waypoint : MonoBehaviour
     public static Transform[] points;
 
     void Awake ()
-    {
-        points = new Transform[transform.childCount];
-        for (int i = 0; 1 < points.Length; i++)
+   {
+      points = new Transform[transform.childCount];
+       for (int i = 0; 1 < points.Length; i++)
         {
             points[i] = transform.GetChild(i);
         }
